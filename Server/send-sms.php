547ab14f7647e428299aa4f6917a876ec13fe78b
Elliot Long <elliot@intohand.com>
2016-06-25 16:47:01 +0100

    Fixed sms

--- Server/send-sms.php
+++ Server/send-sms.php
@@ -6,8 +6,6 @@ $incomingNumber = $_REQUEST['From'];
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-	<Message to="+447855369483" from="<?php echo $incomingNumber?>">
-		<Body>Dale is detained and wants you to call him on <?php echo $incomingNumber?></Body>
-	</Message>
+	<Sms from="+1494372668" to="+447855369483">Dale is detained and wants you to call him on <?php echo $incomingNumber?></Sms>
 	<Say>Your message has been sent</Say>
 </Response>
