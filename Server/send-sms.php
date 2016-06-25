39388cea3df9e0924b5a41e22d810fb764b0142f
Elliot Long <elliot@intohand.com>
2016-06-25 16:37:18 +0100

    Changed to put xml header in php echo

--- Server/send-sms.php
+++ Server/send-sms.php
@@ -3,8 +3,8 @@ header("content-type: text/xml");
 
 $incomingNumber = $_REQUEST['From'];
 
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
-<?xml version="1.0" encoding="UTF-8"?>
 <Response>
 	<Message to="+447855369483" from="<?php echo $incomingNumber?>">
 		<Body>Dale is detained and wants you to call him on <?php echo $incomingNumber?></Body>
