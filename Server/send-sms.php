e5c4dd3bb9d2f7972fb26dcb0b6ae34eacecfa47
Elliot Long <elliot@intohand.com>
2016-06-25 16:51:46 +0100

    sasasas

--- Server/send-sms.php
+++ Server/send-sms.php
@@ -6,6 +6,6 @@ $incomingNumber = $_REQUEST['From'];
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-	<Sms from="+1494372668" to="+447855369483">Dale is detained and wants you to call him on <?php echo $incomingNumber?></Sms>
+	<Sms from="+441494372668" to="+447855369483">Dale is detained and wants you to call him on <?php echo $incomingNumber?></Sms>
 	<Say>Your message has been sent</Say>
 </Response>
