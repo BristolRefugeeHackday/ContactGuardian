d09ec5b14ff97b04ebd7add9c25b2264354b0326
dalepotter <dalepotter@gmail.com>
2016-09-07 00:21:49 +0100

    Implement steps 1 and 2

--- Server/app.py
+++ Server/app.py
@@ -1,12 +1,24 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, session, url_for
+from werkzeug.routing import RequestRedirect
 import twilio.twiml
 
 app = Flask(__name__)
-
+app.secret_key = 'default-secret-key'
 
 # ADD CLASS WHICH STORES LOGIN ATTEMPT NUMBERS ETC
 
-@app.route('/')
+@app.route('/bye', methods=['GET', 'POST'])
+def bye():
+    """Hangup after a number of failed input attemps."""
+
+    resp = twilio.twiml.Response()
+    resp.say("Sorry, I did not hear your response. Please try calling again.")
+    resp.hangup()
+
+    return str(resp)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def step_one():
     """Entry point to respond to incoming requests."""
 
@@ -14,31 +26,76 @@ def step_one():
     with resp.gather(numDigits=6, action="/post_step_one_logic", method="POST") as gather:
     	gather.say("Hello. This is Contact Guardian. Please enter your pin.")
 
+    def process_no_input_response(endpoint, num_retries_allowed=3):
+
+        session['num_retries_allowed'] = num_retries_allowed
+
+        if endpoint in session:
+            session[endpoint] += 1
+        else:
+            session[endpoint] = 1
+
+        if session[endpoint] > num_retries_allowed:
+            # Reached maximum number of retries, so redirect to a message before hanging up
+            raise RequestRedirect(url_for('bye'))
+
+        return resp
+
+    process_no_input_response(request.endpoint)
+    resp.say("Sorry, I did not hear a response. Please try again. Allowed: {}. Tries: {}".format(session['num_retries_allowed'], session['step_one']))
+    resp.redirect(url=url_for('step_one'))
+
     return str(resp)
 
 
 @app.route('/post_step_one_logic', methods=['GET', 'POST'])
 def post_step_one_logic():
-    """Handle response from step 1"""
+    """Handle response from step 1."""
     #FIXME Link up to a database
     # Pin currently set to "123456". Lovely.
 
     digit_pressed = request.values.get('Digits', None)
+    resp = twilio.twiml.Response()
 
     if digit_pressed == "123456":
-    	resp = twilio.twiml.Response()
     	resp.say("Hello {}. You have 3 contacts saved.".format("Dale"))
-    	resp.redirect(url="/step_two")
-    	return str(resp)
-    else:
-    	return redirect(url_for('step_one'))
+    	resp.redirect(url=url_for('step_two'))
+    elif digit_pressed != "123456":
+    	resp.say("Sorry, that pin is not recognaized. Please try again.")
+    	resp.redirect(url=url_for('step_one'))
+    
+    return str(resp)
 
 
-@app.route('/step_two')
+@app.route('/step_two', methods=['GET', 'POST'])
 def step_two():
-    """TODO Step two"""
-    return
+    """Give instructions for alerting a single or all contacts."""
+    
+    resp = twilio.twiml.Response()
+    with resp.gather(numDigits=1, action="/post_step_two_logic", method="POST") as gather:
+    	gather.say("To alert a single contact, press 1. To alert all your contacts press 2")
+
+    resp.say("Sorry, I did not hear a response. Please try again.")
+    resp.redirect(url=url_for('step_two'))
+
+    return str(resp)
+
+
+@app.route('/post_step_two_logic', methods=['GET', 'POST'])
+def post_step_two_logic():
+    """Handle response from step 2"""
+
+    digit_pressed = request.values.get('Digits', None)
+    resp = twilio.twiml.Response()
+
+    if digit_pressed == "1":
+    	resp.redirect(url=url_for('step_three'))
+    elif digit_pressed == "2":
+    	resp.redirect(url=url_for('step_four'))
+	
+	return str(resp)
+
 
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
